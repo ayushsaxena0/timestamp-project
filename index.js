26f01c7e5f36acceff75d0de460d75e32ec51470
Ayush <ayushsaxena917@gmail.com>
2024-06-21 13:53:36 +0530

    Project for freecodecamp certification

--- index.js
+++ index.js
@@ -2,31 +2,43 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api/:date?", getDate);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
+function getDate(req, res) {
+  const date = req.params.date;
+  let dateObj;
+  if (!date) {
+    dateObj = new Date();
+  } else if (date.split("").every((el) => "0123456789".includes(el))) {
+    dateObj = new Date(Number(date));
+  } else {
+    dateObj = new Date(date);
+  }
+  const obj = {
+    unix: dateObj.getTime(),
+    utc: dateObj.toUTCString(),
+  };
+  return res.json(isNaN(obj.unix) ? { error: "Invalid Date" } : obj);
+}
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
